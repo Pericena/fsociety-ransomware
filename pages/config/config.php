63e836b2ede707bc52db3f634df07790dd5e2e05
Pericena <pericena12@gmail.com>
2018-07-31 22:10:08 +0200

    mensaje

---
+++ pages/config/config.php
@@ -0,0 +1,12 @@
+<?php
+$bdd  = new PDO('mysql:host=localhost;dbname=','root','');
+
+//$bdd  = new PDO('mysql:host=localhost;dbname=fsociety','root','');
+
+function get_target(){
+    global $bdd;
+    $select = $bdd->prepare("SELECT * FROM target WHERE 1");
+    $select->execute();
+    $fetch = $select->fetch();
+    return $fetch;
+}
---
+++ pages/fsociety-ransomware.bat
@@ -0,0 +1,232 @@
+@rem bin\windows\bat
+@rem Autor Luishiño Pericena Choque
+@rem Sigueme en las redes Sociales:
+@rem Blogger          https://lpericena.blogspot.com/
+@rem Github            https://github.com/Pericena
+@rem youtube.com  https://www.youtube.com/channel/UCELx1m-NeAdBn7mCuQ86kcw
+@rem pinterest        https://es.pinterest.com/lushiopericena/
+@rem twitter             https://twitter.com/LPericena
+@rem linkedin         https://www.linkedin.com/in/lpericena/
+@rem facebook       https://www.facebook.com/profile.php?id=100009309755063
+@rem pagina facebook  https://www.facebook.com/lpericena
+@rem sitio web        https://pericena.wordpress.com/
+@rem vimeo         https://vimeo.com/lpericena 
+@rem instagram      https://www.instagram.com/lpericena/
+@rem remote      https://remote.com/luishinopericena-choque
+@rem google+   https://plus.google.com/u/0/114054031405340478901
+@rem kiwi       https://kiwi.qa/LuishinoC
+@rem App    https://apps.facebook.com/167466933725219
+@rem Grupo    https://www.facebook.com/groups/122223121705126/?source_id=1506435219407506
+@rem socialtools https://www.socialtools.me/index?action=demoApps&preview=1&app_id=406101
+@rem teachlr    https://teachlr.com/lpericena
+@rem wikipedia  https://es.wikipedia.org/wiki/Usuario:Luishi%C3%B1o_Pericena_Choque
+@rem ask          https://ask.fm/Lpericena
+@rem stackoverflow  https://stackoverflow.com/users/6506592/luishi%C3%B1o-pericena-choque
+@rem wix https://lpericena.wixsite.com/curriculumvitae
+
+
+@echo off
+Color 04
+title Youtube
+rem mode con rate=1000 delay=1000
+mode con cols=100 lines=58
+TIMEOUT /T 0 /nobreak>NUL
+Color 01
+TIMEOUT /T 0 /nobreak>NUL
+Color 02
+echo.                                                             7O85  
+TIMEOUT /T 0 /nobreak>NUL  
+Color 03                     
+echo.                                                            ir  XU 
+TIMEOUT /T 0 /nobreak>NUL   
+Color 04                    
+echo.                                                       :vP@B@u..0P  
+TIMEOUT /T 0 /nobreak>NUL 
+Color 05                     
+echo.                                                  :2E@B@@@   ,7r;   
+TIMEOUT /T 0 /nobreak>NUL 
+Color 06                     
+echo.                                               :qB@BMZOBk        
+TIMEOUT /T 0 /nobreak>NUL 
+Color 07                        
+echo.                                             7B@BZXNXZB2    
+TIMEOUT /T 0 /nobreak>NUL 
+Color 08                             
+echo.                                           .@@OkqkPXEBj     
+TIMEOUT /T 0 /nobreak>NUL 
+Color 09                             
+echo.                                          7@BXkPkXkqB2    
+TIMEOUT /T 0 /nobreak>NUL 
+Color 0a                               
+echo.                                         Y@8kkPkXXqBG    
+TIMEOUT /T 0 /nobreak>NUL 
+Color 0b                                
+echo.                                        ,@OFSPkXkPO@    
+TIMEOUT /T 0 /nobreak>NUL       
+Color 0c                           
+echo.                .,720M@B@B@B@OSv:       @@SFXkPSPP@:                  ...              
+TIMEOUT /T 0 /nobreak>NUL   
+Color 0d
+echo.           .iSM@@@BMO80ZZGE88BB@@@Bq:  vBNFXkXkXSM8           :vkOM@B@B@B@BMGk7.         
+TIMEOUT /T 0 /nobreak>NUL 
+Color 0e
+echo.  ..    :N@B@@@B@B@BMOOqXFSSPkPXPPMB@BSB@5kSXkPkPB7       :5BB@BM8ZNNXN0MB@B@@@@@@u.,  .  
+TIMEOUT /T 0 /nobreak>NUL
+Color 0f
+echo.:77;vkU@B@@@@@B@B@B@B@B@B@Ek5SSXkPXXSMB@O5SXXXkXk@.    70@@BG0qqkkSNM@B@@@Bk5SuuUqGMB@r:vi
+TIMEOUT /T 0 /nobreak>NUL
+Color 01
+echo.@X  :Bu,               .2B@BZ1kSXkXSX1kBB1SSkFkSkB. :q@BMENPqXX5SZ@B@87              B  .M
+TIMEOUT /T 0 /nobreak>NUL
+Color 01
+echo.,XUYu.                    8@BMFSSXkXkk1S0EqPkXFXkMM@@MPPkPkXSS1GB@0:                 ;uJML
+TIMEOUT /T 0 /nobreak>NUL
+Color 02
+echo.  ..                       B@B8u55S5SSXSSqOOMOMOMMMMMGEFSFS515BB@                      .  
+TIMEOUT /T 0 /nobreak>NUL
+Color 03
+echo.                            @@BOXPkqXP5F5S5XkqkXFF1510OM88GZ0@B@                       
+TIMEOUT /T 0 /nobreak>NUL   
+Color 04
+echo.                            F@@B@B@@@BM0X5SFXkXSXFSE80qXNNOB@B@                    
+TIMEOUT /T 0 /nobreak>NUL   
+Color 05    
+echo.                             @B@B@XPNOM@BMqkFXkXF0O8kkSk5FUSB@G                
+TIMEOUT /T 0 /nobreak>NUL   
+Color 06        
+echo.                             B@B@B1u2211XZMO0SXSqZq5kSk1SFSU@@:               
+TIMEOUT /T 0 /nobreak>NUL  
+Color 07          
+echo.                             @@@M@BEXS5SSSFqZGkXXkSXFSXGG8NS@@                        
+TIMEOUT /T 0 /nobreak>NUL 
+Color 08   
+echo.                             B@ZZO@B@B@8qSXFSqZkXFXZ@B@@@MOk@M                    
+TIMEOUT /T 0 /nobreak>NUL 
+Color 09       
+echo.                             @B@M@r iu0@BZFSFSkP50@@85r i@8MB7                      
+TIMEOUT /T 0 /nobreak>NUL
+Color 0a      
+echo.                             B@OOB1      MBPkFk5Z@,     ;BOM@r                
+TIMEOUT /T 0 /nobreak>NUL  
+Color 0b          
+echo.                             @B@XGB@Ev;LP@BBMBOMB@7::r1@BOqPBu             
+TIMEOUT /T 0 /nobreak>NUL
+Color 0c               
+echo.                             B@MMqkNBB@B@GBB@B@@@M@B@B@EPXOM@B              
+TIMEOUT /T 0 /nobreak>NUL  
+Color 0d            
+echo.                             @i :kMMEEqNN8BZ:::uBMPEEOZOO8L  ZB       
+TIMEOUT /T 0 /nobreak>NUL 
+Color 0e                   
+echo.                            ,B  7XvX@@BEUOY.    :7;r58BMU78j. @      
+TIMEOUT /T 0 /nobreak>NUL 
+Color 05                    
+echo.                             @i i@Y   :L  ,v        J   .0@. ik    
+TIMEOUT /T 0 /nobreak>NUL 
+Color 02                      
+echo.                             LB  5rMY  .vi        2r  ,kP,U  Z    
+TIMEOUT /T 0 /nobreak>NUL 
+Color 0a                       
+echo.                              @i 7  B5OZB@B :   r@@@OGB: .: 2r          
+TIMEOUT /T 0 /nobreak>NUL 
+Color 0b                 
+echo.                               M. , N2. r 5Bj  v@ i  jU  , Lr            
+TIMEOUT /T 0 /nobreak>NUL 
+Color 0c                
+echo.                                2:  G@B@0,::Z@BMr 2MB@q  iu:        
+TIMEOUT /T 0 /nobreak>NUL 
+Color 01                     
+echo.                                 i7 :BGE@@@B@B@B@B@GZB. ;v        
+TIMEOUT /T 0 /nobreak>NUL 
+Color 01                       
+echo.                                   vOEiEB1Nk5M15qUMB;Zqu,  %OS%
+TIMEOUT /T 0 /nobreak>NUL  
+Color 02                          
+echo.                                    ,Or :5BMEOqO@O7 ,Br   %USERNAME%
+TIMEOUT /T 0 /nobreak>NUL 
+Color 03                   
+echo.                                      ,7..,:7U;::  .:               
+TIMEOUT /T 0 /nobreak>NUL 
+Color 04                     
+echo.                                        72uiir7:;r:               
+TIMEOUT /T 0 /nobreak>NUL 
+Color 05                     
+echo.                                           :,.,.i:              
+TIMEOUT /T 0 /nobreak>NUL 
+Color 06                           
+echo.                                            , .                      
+TIMEOUT /T 0 /nobreak>NUL
+Color 07
+echo.                                        #OPdailyallowance.
+TIMEOUT /T 0 /nobreak>NUL
+Color 04
+echo.                                       Your files are encrypted.
+TIMEOUT /T 0 /nobreak>NUL
+echo.To get the key to decrypt files, you have to paid 5.9 million USD. if payment is not made by tommorow night we'll brick your entire system.
+TIMEOUT /T 0 /nobreak>NUL
+echo.                           More instructions forthcoming. - fsociety.
+tree>null
+TIMEOUT /T 0 /nobreak>NUL
+systeminfo>null
+TIMEOUT /T 0 /nobreak>NUL
+echo.                                   Time left: %Time% Date:%date%
+TIMEOUT /T 0 /nobreak>NUL
+echo.             Autor: Luishino Pericena Choque   https://lpericena.blogspot.com
+TIMEOUT /T 0 /nobreak>NUL
+echo.----------------------------------------------------------------------------------------------------
+echo.Your system :%COMPUTERNAME%: First connect IP :192.168.0.3 :flagTotal encrypted : encrypted files.
+echo.----------------------------------------------------------------------------------------------------
+echo.                Refresh  Payment  FAQ  Decrypt one file for free  Support
+echo.                                    JΞSTΞR ✪ ΔCTUAL³³º¹ 
+echo. There's an unequal amount of good and bad in most things. The trick is to figure out the ratio and act accordingly.
+del null
+echo fsociety-ransomware.hta %ruta%
+fsociety-ransomware.bat 
+echo index.html %ruta%
+
+pause
+
+
+echo --yes-playlist	Descargar videos de una lista.
+echo --max-filesize SIZE	No descargar videos que sobrepasen un tamaño especifico. Sustituye SIZE por el tamaño máximo. Por ejemplo 100 k o 100B.
+echo --min-views COUNT	No descargar los videos que se hayan visto la cantidad de veces especificada. Sustituye COUNT por el número mínimo de vistas de los videos.
+echo --dateafter now-1months	Descargar solo videos recientes, en este caso del último mes.
+echo -r LIMIT	Limitar la velocidad de descarga de los videos, para que no interfieran en otras tareas. Sustituye LIMIT por la velocidad. Por ejemplo: 30K o 1M.
+echo --batch-file FILE	Descargar una lista de videos con distintas direcciones URL, que se deben listar en un archivo de texto. Sustituye FILE por el nombre del archivo.                                                                                                                                       
+
+
+
+
+
+
+echo.																PPu                   
+echo.															  ;Z  M.                  
+echo.														:75B@Njv7ru 
+echo.													 rN@B@B@i 
+echo.												   u@BM00M@,                          
+echo.												 :B@EqPqZ@.                           
+echo.												rBMkPSPN@:                            
+echo.											   .BOSPkXP@J     
+echo.						   .iv1EBB@B@B@ONJ:    B@5PSXkOM           .::rrri:           
+echo.					   :UBB@B@B@MO00qE0OM@B@Mr:@XkkPSqBr      ,LkMB@@@B@B@@@B@Z7      
+echo.				,:iiv1@B@B@B@B@B@B@BOkSSqXqqMB@@FFPkPk@    iSB@@MZ0P8B@B@B@OMO@BOGBii:
+echo.				BF rB7            .kB@EFFPkXFSO@FSSk5kM. 5B@MGPqFX8@BBv.          8,.O
+echo.				 iLi                .B@Z21SSk5FkZZE0EEMB@BZkXSk1q@@J               ru:
+echo.									 :B@NFFSSk1FS0EGEZqPkGOG00XE@@                    
+echo.									  @@@B@B@BOPF1SFk5FkZqNqEE@B@.                    
+echo.									  r@@@8FXZOMOZSXSkNOqX5F22X@B    
+echo.									   @@BB51U5FNZOqXk0Sk5kXNkP@r                     
+echo.									   @BOB@B@M05SkZPXFNO@B@BON@     
+echo.									   B@BM .71XEFF5kF08kL: 8B@@    
+echo.									   @BMB7    B@EZ0O@    :OMB@                      
+echo.									   B@ZG@@M@B@B@B@B@MMO@BGEBB.  
+echo.									  r@ 7Z@BB8OB@7ii@@OM@MBM5.:B                     
+echo.									  8M jZ;2POrU7    i.vSF7LM7 B                     
+echo.									  .@ :Br  .         .  :YM  u                     
+echo.									   5; :.@LYB@.    @BPYM: : U                      
+echo.										1.  F0.i.q2 .0:i S1  ,7.                      
+echo.										 ri u@@BS2O@@OuOMBJ :v  
+echo.										  .:5GF@@@B@B@B@08Li:                         
+echo.											rS.:5ZNOXOE7.kU                           
+echo.											  ,::;7J;i.                               
+echo.											   .riii::7 
